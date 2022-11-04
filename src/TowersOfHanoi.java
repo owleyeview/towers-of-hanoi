6bc92f24097b9b200760db1aef7458f7d5fa41d7
owleyeview <rashaanlightpool@gmail.com>
2022-11-04 16:06:12 -0700

    finished

--- src/TowersOfHanoi.java
+++ src/TowersOfHanoi.java
@@ -5,15 +5,14 @@ public class TowersOfHanoi {
         playTowers(discs, 'A', 'B', 'C');
     }
 
-    public static void playTowers(int n, char from, char aux, char to) throws Exception {
+    public static void playTowers(int n, char fromPeg, char auxPeg, char toPeg) throws Exception {
         if (n < 0) throw new IllegalArgumentException();
         if (n == 1) {
-            System.out.println("Move disk 1 from " + from + " to " + to);
+            System.out.println("Move disk 1 from " + fromPeg + " to " + toPeg);
         } else {
-            playTowers(n - 1, , , );
-            System.out.println("Move disk " + n + " from " + from + " to " + to);
-            playTowers(n - 1, , , );
+            playTowers(n - 1, fromPeg, toPeg, auxPeg);
+            System.out.println("Move disk " + n + " from " + fromPeg + " to " + toPeg);
+            playTowers(n - 1, auxPeg, fromPeg, toPeg);
         }
     }
-    
 }
